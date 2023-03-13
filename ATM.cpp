b391da2a10f887790d2410229787c276e02bd54f
Tiia Nuutinen <tiiapirita@gmail.com>
2023-03-13 11:09:41 +0200

    Add project files.

---
+++ ATM.cpp
@@ -0,0 +1,53 @@
+// ATM.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+    cout << "**********MENU**********" << endl;
+    cout << "1. Check balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
+    cout << "************************" << endl;
+}
+
+int main()
+{
+    int option;
+    double balance = 500;
+
+    do {
+        showMenu();
+        cout << "Option: ";
+        cin >> option;
+        system("cls");
+
+        switch (option) {
+        case 1:
+            cout << "Balance is: " << balance << "€" << endl;
+            break;
+        case 2:
+            cout << "Deposit amount: ";
+            double depositAmount;
+            cin >> depositAmount;
+            balance += depositAmount;
+            break;
+        case 3:
+            cout << "Withdraw amount: ";
+            double withdrawAmount;
+            cin >> withdrawAmount;
+            if (withdrawAmount <= balance) {
+                balance -= withdrawAmount;
+            } else {
+                cout << "Not enough money";
+            }
+            break;
+        }
+    } while (option != 4);
+    
+
+    system("pause>0");
+}
+
